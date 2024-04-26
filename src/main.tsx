7be89f4f460e995efae838356df1d5abba8a046f
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-26 20:41:59 +0200

    update queryClient error handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import router from './router/index'
+import { toast } from 'react-toastify'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast(`Something went wrong: ${error.message}`)
+    }
+  })
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
