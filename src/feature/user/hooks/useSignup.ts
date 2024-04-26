58785012eec4490d5d87da1c624caef9abd71031
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-26 17:06:39 +0200

    add useSignup hook

---
+++ src/feature/user/hooks/useSignup.ts
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import { signup } from '../../../service/user.service'
+import type { TSignupInput } from '../zod/user.zodSchema'
+
+/**
+ * Custom hook for signing up a user.
+ * @returns A mutation function that can be used to sign up a user.
+ */
+export function useSignup() {
+  const navigate = useNavigate()
+
+  return useMutation({
+    mutationFn: (userSignupData: TSignupInput) => signup(userSignupData),
+    onSuccess: () => {
+      // Notify user
+      toast.success('You signed up successfully!')
+      // Nav to home page
+      navigate('/')
+    }
+  })
+}
