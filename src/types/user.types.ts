146b71041dc817a4a85cbe034b5cc85eb19f7341
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-26 17:07:08 +0200

    add user type

---
+++ src/types/user.types.ts
@@ -0,0 +1,10 @@
+export type TUserRole = 'admin' | 'lead-guide' | 'guide' | 'user'
+
+export type TUser = {
+  _id: string
+  name: string
+  email: string
+  photo?: string
+  role: TUserRole
+  isActive: boolean
+}
