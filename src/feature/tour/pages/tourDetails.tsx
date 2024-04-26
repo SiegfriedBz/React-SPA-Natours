0a8ce3a7839a6de08e9f7154984ce0448bebc5f8
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-26 17:54:15 +0200

    add tour pages

---
+++ src/feature/tour/pages/tourDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const tourDetails = () => {
+  return <div>tourDetails</div>
+}
+
+export default tourDetails
---
+++ src/feature/tour/pages/updateTour.tsx
@@ -0,0 +1,12 @@
+import UpdateTourForm from '../components/UpdateTourForm'
+
+const updateTour = () => {
+  return (
+    <div>
+      updateTour
+      <UpdateTourForm />
+    </div>
+  )
+}
+
+export default updateTour
