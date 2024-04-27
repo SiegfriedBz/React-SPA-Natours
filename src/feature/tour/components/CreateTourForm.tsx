565e3ccf5540acd393f495301f876c95631bfa7f
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-27 14:14:28 +0200

    cleanup

--- src/feature/tour/components/CreateTourForm.tsx
+++ src/feature/tour/components/CreateTourForm.tsx
@@ -19,7 +19,6 @@ const CreateTourForm = () => {
   const { mutate } = useMutateTour()
 
   const onSubmit: SubmitHandler<TCreateTourInput> = (data) => {
-    console.log(data)
     mutate(data)
   }
 
@@ -37,58 +36,53 @@ const CreateTourForm = () => {
       <input {...register('maxGroupSize')} />
       {errors.maxGroupSize && <span>{errors.maxGroupSize.message}</span>}
 
-      <label>price</label>
+      <label>Price</label>
       <input {...register('price')} />
       {errors.price && <span>{errors.price.message}</span>}
 
-      <label>summary</label>
+      <label>Summary</label>
       <input {...register('summary')} />
       {errors.summary && <span>{errors.summary.message}</span>}
 
-      <label>description</label>
+      <label>Description</label>
       <input {...register('description')} />
       {errors.description && <span>{errors.description.message}</span>}
 
-      <label>difficulty</label>
+      <label>Difficulty</label>
       <input {...register('difficulty')} />
       {errors.difficulty && <span>{errors.difficulty.message}</span>}
 
-      <label>discount</label>
+      <label>Discount</label>
       <input {...register('discount')} />
       {errors.discount && <span>{errors.discount.message}</span>}
 
-      <label>imageCover</label>
-      <input {...register('imageCover')} type='file' />
+      <label>Image Cover</label>
+      <input {...register('imageCover')} type="file" />
       {errors.imageCover && <span>{errors.imageCover.message}</span>}
 
-      <label>images</label>
-      <input {...register('images')} type='file' multiple />
+      <label>Images</label>
+      <input {...register('images')} type="file" multiple />
       {errors.images && <span>{errors.images.message}</span>}
 
-      <label>discount</label>
-      <input {...register('discount')} />
-      {errors.discount && <span>{errors.discount.message}</span>}
-
       {/* Input fields for startLocation */}
-      <label>startLocation</label>
-
+      <label>Start Location</label>
       <input
         {...register('startLocation.coordinates.0')}
-        placeholder='Longitude'
+        placeholder="Longitude"
       />
       {errors.startLocation && (
         <span>{errors.startLocation.coordinates?.[0]?.message}</span>
       )}
       <input
         {...register('startLocation.coordinates.1')}
-        placeholder='Latitude'
+        placeholder="Latitude"
       />
       {errors.startLocation && (
         <span>{errors.startLocation.coordinates?.[1]?.message}</span>
       )}
       <input
         {...register('startLocation.description')}
-        placeholder='Description'
+        placeholder="Description"
       />
       {errors.startLocation && (
         <span>{errors.startLocation.description?.message}</span>
@@ -98,26 +92,26 @@ const CreateTourForm = () => {
       <label>Locations</label>
       <input
         {...register('locations.0.coordinates.0')}
-        placeholder='Longitude'
+        placeholder="Longitude"
       />
       {errors.locations && errors.locations[0] && (
         <span>{errors.locations[0].coordinates?.[0]?.message}</span>
       )}
       <input
         {...register('locations.0.coordinates.1')}
-        placeholder='Latitude'
+        placeholder="Latitude"
       />
       {errors.locations && errors.locations[0] && (
         <span>{errors.locations[0].coordinates?.[1]?.message}</span>
       )}
       <input
         {...register('locations.0.description')}
-        placeholder='Description'
+        placeholder="Description"
       />
       {errors.locations && errors.locations[0] && (
         <span>{errors.locations[0].description?.message}</span>
       )}
-      <input {...register('locations.0.day')} placeholder='Day' />
+      <input {...register('locations.0.day')} placeholder="Day" />
       {errors.locations && errors.locations[0] && (
         <span>{errors.locations[0].day?.message}</span>
       )}
@@ -127,7 +121,7 @@ const CreateTourForm = () => {
       <input {...register('guides')} />
       {errors.guides && <span>{errors.guides.message}</span>}
 
-      <button type='submit'>Create Tour</button>
+      <button type="submit">Create Tour</button>
     </form>
   )
 }
