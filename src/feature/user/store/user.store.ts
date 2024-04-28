a068eda7ed5ea71467dc536fdfceb3f8b0084a12
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-28 16:13:30 +0200

    update user store: add setUserPosition + getUserPosition + getPositionStatus

--- src/feature/user/store/user.store.ts
+++ src/feature/user/store/user.store.ts
@@ -1,12 +1,50 @@
 import { create } from 'zustand'
-import { TUser } from '../../../types/user.types'
+import getPosition from '../../../utils/getPosition'
+import type { TUser } from '../../../types/user.types'
 
-type Store = {
+type TState = {
   user: TUser | null
-  setUser: (userData: TUser | null) => void
+  getPositionStatus: 'idle' | 'loading' | 'error'
 }
+type TActions = {
+  setUser: (userData: TUser | null) => void
+  // set user position from user input
+  setUserPosition: (latLng: string) => void
+  // set user position from navigator
+  getUserPosition: () => Promise<void>
+}
+type TStore = TState & TActions
 
-export const useUserStore = create<Store>()((set) => ({
+export const useUserStore = create<TStore>()((set, get) => ({
   user: null,
-  setUser: (userData) => set(() => ({ user: userData }))
+  getPositionStatus: 'idle',
+  setUser: (userData) => set(() => ({ user: userData })),
+  setUserPosition: (latLng: string) => {
+    const user = get().user
+    if (user && user._id) {
+      set({
+        user: { ...user, latLng }
+      })
+    }
+  },
+  getUserPosition: async () => {
+    try {
+      const user = get().user
+      if (user && user._id) {
+        set({
+          getPositionStatus: 'loading'
+        })
+        // Get the user's geolocation position
+        const position = await getPosition()
+        const latLng = `${position?.coords?.latitude},${position?.coords?.longitude}`
+
+        set({
+          user: { ...user, latLng },
+          getPositionStatus: 'idle'
+        })
+      }
+    } catch (error) {
+      set({ getPositionStatus: 'error' })
+    }
+  }
 }))
