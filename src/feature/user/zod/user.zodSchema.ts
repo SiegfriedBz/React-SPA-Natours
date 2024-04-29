fad687644448239f28269175937fbf6787138126
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-29 07:45:41 +0200

    add reset password zod schemas

--- src/feature/user/zod/user.zodSchema.ts
+++ src/feature/user/zod/user.zodSchema.ts
@@ -27,5 +27,36 @@ const signupZodSchema = z
 
 type TSignupInput = z.TypeOf<typeof signupZodSchema>
 
-export { signupZodSchema }
-export type { TSignupInput }
+const forgotMyPasswordZodSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required'
+    })
+    .email('Not a valid email')
+})
+
+type TForgotMyPasswordInput = z.TypeOf<typeof forgotMyPasswordZodSchema>
+
+const resetMyPasswordZodSchema = z
+  .object({
+    password: z
+      .string({
+        required_error: 'Password is required'
+      })
+      .min(6, 'Password too short - should be 6 chars minimum'),
+    passwordConfirmation: z.string({
+      required_error: 'passwordConfirmation is required'
+    }),
+    resetPasswordToken: z.string({
+      required_error: 'resetPasswordToken is required'
+    })
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'Passwords do not match',
+    path: ['passwordConfirmation']
+  })
+
+type TResetMyPasswordInput = z.TypeOf<typeof resetMyPasswordZodSchema>
+
+export { signupZodSchema, forgotMyPasswordZodSchema, resetMyPasswordZodSchema }
+export type { TSignupInput, TForgotMyPasswordInput, TResetMyPasswordInput }
