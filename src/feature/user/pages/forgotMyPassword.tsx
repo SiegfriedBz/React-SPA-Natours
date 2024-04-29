15f2ed11813b72afe1852556c8ce51d440948f4e
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-29 07:46:33 +0200

    add reset password pages + forms

---
+++ src/feature/user/pages/forgotMyPassword.tsx
@@ -0,0 +1,12 @@
+import ForgotPasswordForm from '../components/ForgotPasswordForm'
+
+const forgotMyPassword = () => {
+  return (
+    <div>
+      Reset my password - 1/2
+      <ForgotPasswordForm />
+    </div>
+  )
+}
+
+export default forgotMyPassword
