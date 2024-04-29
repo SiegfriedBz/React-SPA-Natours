8a45fcff013eefe51a90c6b084d2f8f34ddeade3
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-29 14:17:39 +0200

    add /me route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 import AppLayout from '../ui/layout/AppLayout'
-import Login from '../feature/auth/pages/login'
 import Signup from '../feature/user/pages/signup'
+import Login from '../feature/auth/pages/login'
+import UserProfile from '../feature/user/pages/userProfile'
 import ForgotMyPassword from '../feature/user/pages/forgotMyPassword'
 import ResetMyPassword from '../feature/user/pages/resetMyPassword'
 import Tours from '../feature/tour/pages/tours'
 import Tour from '../feature/tour/pages/tour'
-import CreateTourForm from '../feature/tour/components/CreateTourForm'
-import UpdateTourForm from '../feature/tour/components/UpdateTourForm'
+import CreateTour from '../feature/tour/pages/createTour'
+import UpdateTour from '../feature/tour/pages/updateTour'
 import Stats from '../feature/tour/pages/stats'
 import MonthlyStats from '../feature/tour/pages/monthlyStats'
 import About from '../ui/pages/about'
@@ -21,15 +22,16 @@ const router = createBrowserRouter([
       { path: '/tours', element: <Navigate to="/" replace={true} /> },
       { path: '/login', element: <Login /> },
       { path: '/signup', element: <Signup /> },
+      { path: '/me', element: <UserProfile /> },
       { path: '/resetMyPassword-1/2', element: <ForgotMyPassword /> },
       {
         path: '/resetMyPassword-2/2/:resetPasswordToken',
         element: <ResetMyPassword />
       },
       { path: '/about', element: <About /> },
-      { path: '/tours/new', element: <CreateTourForm /> },
+      { path: '/tours/new', element: <CreateTour /> },
       { path: '/tours/:tourId', element: <Tour /> },
-      { path: '/tours/:tourId/update', element: <UpdateTourForm /> },
+      { path: '/tours/:tourId/update', element: <UpdateTour /> },
       { path: '/tours/stats', element: <Stats /> },
       { path: '/tours/stats/:year', element: <MonthlyStats /> }
     ]
