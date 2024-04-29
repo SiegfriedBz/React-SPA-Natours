635687c9fee27e59baad3101eb36a20a55ce9878
siegfriedbz <siegfried.bozza@yahoo.com>
2024-04-29 14:22:22 +0200

    add getMe / updateMe / updateMe / deleteMe services

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,10 +2,33 @@ import logger from '../utils/logger.utils'
 import type {
   TSignupInput,
   TForgotMyPasswordInput,
-  TResetMyPasswordInput
+  TResetMyPasswordInput,
+  TUpdateMeInput,
+  TUpdateMyPasswordInput
 } from '../feature/user/zod/user.zodSchema'
 
 const API_URL = import.meta.env.VITE_API_URL
+
+export async function getMe() {
+  try {
+    const response = await fetch(`${API_URL}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    })
+    const data = await response.json()
+
+    if (!response.ok || data.status !== 'success') {
+      throw new Error(`Getting user profile went wrong`)
+    }
+
+    return data
+  } catch (error) {
+    logger.info(error)
+    throw error
+  }
+}
 
 export async function signup(inputSignup: TSignupInput) {
   try {
@@ -20,6 +43,80 @@ export async function signup(inputSignup: TSignupInput) {
 
     if (!response.ok || data.status !== 'success') {
       throw new Error(`Signup went wrong`)
+    }
+
+    return data
+  } catch (error) {
+    logger.info(error)
+    throw error
+  }
+}
+
+export async function updateMe(mutateUserData: TUpdateMeInput) {
+  try {
+    // Prepare form data as multipart formdata
+    const formData = new FormData()
+    Object.entries(mutateUserData).forEach(([key, value]) => {
+      if (key === 'photo' && value instanceof FileList) {
+        // If the value is a FileList (from an input type="file"), append the first file
+        formData.append(key, value[0])
+      } else {
+        // Convert other non-string values to string
+        formData.append(key, String(value))
+      }
+    })
+
+    const response = await fetch(`${API_URL}/users/update-me`, {
+      method: 'PATCH',
+      credentials: 'include',
+      body: formData
+    })
+    const data = await response.json()
+
+    if (!response.ok || data.status !== 'success') {
+      throw new Error(`Profile update went wrong`)
+    }
+
+    return data
+  } catch (error) {
+    logger.info(error)
+    throw error
+  }
+}
+
+export async function updateMyPassword(mutateUserData: TUpdateMyPasswordInput) {
+  try {
+    const response = await fetch(`${API_URL}/users/update-my-password`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(mutateUserData)
+    })
+    const data = await response.json()
+
+    if (!response.ok || data.status !== 'success') {
+      throw new Error(`Password update went wrong`)
+    }
+
+    return data
+  } catch (error) {
+    logger.info(error)
+    throw error
+  }
+}
+
+export async function deleteMe() {
+  try {
+    const response = await fetch(`${API_URL}/users/delete-me`, {
+      method: 'PATCH',
+      credentials: 'include'
+    })
+    const data = await response.json()
+
+    if (!response.ok || data.status !== 'success') {
+      throw new Error(`Password update went wrong`)
     }
 
     return data
