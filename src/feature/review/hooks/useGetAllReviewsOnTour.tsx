87cddc378d839e3c67e9523c7c0bdeccde5fa425
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-02 18:36:29 +0200

    add review hooks

---
+++ src/feature/review/hooks/useGetAllReviewsOnTour.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAllReviewsOnTour } from '../../../service/review.service'
+
+type TProps = {
+  tourId: string
+}
+
+export function useGetAllReviewsOnTour({ tourId }: TProps) {
+  const { status, isPending, isSuccess, isError, isLoading, data, refetch } =
+    useQuery({
+      queryKey: ['reviews'],
+      queryFn: () => getAllReviewsOnTour(tourId)
+    })
+
+  return { data, refetch, status, isPending, isSuccess, isError, isLoading }
+}
