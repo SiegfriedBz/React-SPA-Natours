4be089fae56aff7bf01cef3b9a85d0bf56ff57d1
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-19 18:16:04 +0200

    refactor booking components

--- src/feature/booking/components/AllMyBookings.tsx
+++ src/feature/booking/components/AllMyBookings.tsx
@@ -1,51 +1,75 @@
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import { useNavigate, Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import useUserStore from '../../user/store/user.store'
+import { useGetMe } from '../../user/hooks/useGetMe'
 import { useGetMyBookings } from '../hooks/useGetMyBookings'
 import BookingCard from './BookingCard'
+import BookingCardSkeleton from '../../../ui/components/skeleton/BookingCardSkeleton'
 import type { TBooking } from '../../../types/booking.types'
 
 const AllMyBookings = () => {
+  const navigate = useNavigate()
+  const setUser = useUserStore((state) => state.setUser)
+
+  // necessary to fetch user after redirect from stripe checkout complete
   const {
-    data,
-    refetch,
-    // status, isPending, isSuccess,
-    isError,
-    isLoading
-  } = useGetMyBookings()
+    data: dataUser,
+    isError: isErrorUser,
+    isLoading: isLoadingUser
+  } = useGetMe()
 
-  if (isLoading) {
-    return (
-      <div>
-        <div className="h-56 w-64">
-          <SkeletonTheme baseColor="#55c57a" highlightColor="#43a263">
-            <p>
-              <Skeleton count={3.5} containerClassName="flex-1" />
-            </p>
-          </SkeletonTheme>
-        </div>
-      </div>
-    )
-  }
+  const { data, isError, isLoading } = useGetMyBookings()
 
-  if (isError) {
-    return (
-      <div>
-        <div>Error fetching data</div>
-        <button type="button" onClick={() => refetch()}>
-          Retry
-        </button>
-      </div>
-    )
-  }
+  useEffect(() => {
+    if (dataUser?.data?.user == null) return
+
+    // update user in ui state after fetch
+    setUser(dataUser?.data?.user)
+  }, [setUser, dataUser?.data?.user])
 
   const bookings: TBooking[] = data?.data?.bookings
 
-  return (
-    <>
+  if (isLoadingUser || isLoading) {
+    return (
+      <div className="cards-grid">
+        <LoadingSkeletons />
+      </div>
+    )
+  }
+
+  if (isErrorUser || isError) {
+    return <Navigate to="/login" replace={true} />
+  }
+
+  return bookings?.length === 0 ? (
+    <div>
+      <div className="text-center p-4">
+        <p className="mb-8 text-lg font-semibold text-stone-700">
+          You haven&apos;t booked a tour yet! Start your adventure by exploring
+          our exciting tour options today.
+        </p>
+        <button onClick={() => navigate('/')} className="btn-xl btn-primary">
+          Explore Tours
+        </button>
+      </div>
+    </div>
+  ) : (
+    <div className="cards-grid">
       {bookings?.map((booking) => {
         return <BookingCard key={booking?._id} booking={booking} />
       })}
-    </>
+    </div>
   )
 }
 
 export default AllMyBookings
+
+const LoadingSkeletons = () => {
+  return (
+    <>
+      {Array.from({ length: 6 }, (_, idx) => {
+        return <BookingCardSkeleton key={idx} />
+      })}
+    </>
+  )
+}
--- src/feature/booking/components/BookingCard.tsx
+++ src/feature/booking/components/BookingCard.tsx
@@ -1,15 +1,121 @@
+import { Link } from 'react-router-dom'
+import ModalProvider from '../../../ui/components/modal/Modal'
+import CreateReviewForm from '../../review/components/CreateReviewForm'
+import MotionCardWrapper from '../../../ui/components/MotionCardWrapper'
+import SVGIcon from '../../../ui/components/SVGIcon'
 import type { TBooking } from '../../../types/booking.types'
+
+const API_PUBLIC_URL = import.meta.env.VITE_API_PUBLIC_URL
 
 type TProps = {
   booking: TBooking
 }
 
 const BookingCard = ({ booking }: TProps) => {
+  const bookingPrice = booking.price
+  const {
+    _id: tourId,
+    name: tourName,
+    // slug,
+    imageCover,
+    difficulty,
+    duration,
+    locations,
+    maxGroupSize,
+    ratingsAverage,
+    ratingsCount
+  } = booking.tour
+
   return (
-    <div>
-      <h1>BookingCard</h1>
-      {JSON.stringify(booking)}
-    </div>
+    <MotionCardWrapper>
+      <div data-cy="booking-card" className="card">
+        {/* card header */}
+        <div className="relative h-64">
+          {imageCover && (
+            <div className="absolute inset-0 u-clip-path-img">
+              <div className="absolute w-full h-full u-bg-gradient-primary">
+                <img
+                  className="object-cover h-full w-full opacity-85"
+                  src={`${API_PUBLIC_URL}/img/tours/${imageCover}`}
+                  alt={`${tourName}`}
+                />
+              </div>
+            </div>
+          )}
+          <Link to={`/tours/${tourId}`} className="absolute right-3 bottom-3">
+            <h3 className="h3 rounded-xl ring-2 ring-stone-100">
+              <span>{tourName}</span>
+            </h3>
+          </Link>
+        </div>
+
+        {/* card details */}
+        <div className="p-4">
+          <h4 className="text-sm uppercase font-bold my-2">{`${difficulty} ${duration}-day tour`}</h4>
+
+          <div className="mt-4">
+            <div className="grid grid-cols-2 justify-items-start">
+              <div className="h-8 text-primary flex items-center space-x-2">
+                <SVGIcon iconName="flag" />
+                <span className="text-sm text-stone-800">
+                  {locations?.length || 1}{' '}
+                  {locations?.length > 2 ? 'stops' : 'stop'}
+                </span>
+              </div>
+              <div className="h-8 text-primary flex items-center space-x-2">
+                <SVGIcon iconName="user" />
+                <span className="text-sm text-stone-800">
+                  {maxGroupSize} people
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* card footer */}
+        <div
+          className="p-4
+          bg-stone-100
+          flex items-center justify-between
+          "
+        >
+          <div className="flex flex-col gap-y-2 pl-1">
+            <p>
+              <span className="font-bold">${bookingPrice}</span>{' '}
+            </p>
+            <p>
+              <span className="font-bold">{ratingsAverage}</span>{' '}
+              <span>{`rating (${ratingsCount})`}</span>
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center gap-y-1">
+            {/* Tour link */}
+            <Link className="btn btn-primary" to={`/tours/${tourId}`}>
+              Details
+            </Link>
+
+            {/* Create review button/modal */}
+            <ModalProvider>
+              <ModalProvider.OpenButton
+                className="btn-primary btn-submit"
+                windowNameToOpen="create-review"
+              >
+                <span>Review this tour</span>
+              </ModalProvider.OpenButton>
+              <ModalProvider.Window windowNameToOpen="create-review">
+                <CreateReviewForm
+                  tourId={tourId}
+                  tourName={tourName}
+                  tourImageCover={imageCover}
+                  tourCurrentRatingsAverage={ratingsAverage}
+                />
+              </ModalProvider.Window>
+            </ModalProvider>
+          </div>
+        </div>
+      </div>
+    </MotionCardWrapper>
   )
 }
 
