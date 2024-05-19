5d1bd6472f1cd1340f5bbd519246d8b98ec2341c
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-19 18:02:58 +0200

    add UserAvatar

---
+++ src/feature/user/components/UserAvatar.tsx
@@ -0,0 +1,28 @@
+import { twMerge } from 'tailwind-merge'
+import useUserStore from '../store/user.store'
+import userDefaultImage from '../../../assets/user/default.jpg'
+import type { TUser } from '../../../types/user.types'
+
+const API_PUBLIC_URL = import.meta.env.VITE_API_PUBLIC_URL
+
+type TProps = {
+  className?: string
+}
+
+const UserAvatar = ({ className = '' }: TProps) => {
+  const user: TUser | null = useUserStore((state) => state.user)
+
+  return (
+    <img
+      src={
+        user?.photo
+          ? `${API_PUBLIC_URL}/img/users/${user.photo}`
+          : userDefaultImage
+      }
+      alt="user profile"
+      className={twMerge(`rounded-full object-cover w-16 h-16`, className)}
+    />
+  )
+}
+
+export default UserAvatar
