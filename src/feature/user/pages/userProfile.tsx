78741a1f5a6383b85aca62460b2d8bac666ef7a7
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-22 12:17:14 +0200

    update UserProfile to use currentUser if exist

--- src/feature/user/pages/userProfile.tsx
+++ src/feature/user/pages/userProfile.tsx
@@ -9,22 +9,24 @@ import SkeletonForm from '../../../ui/components/skeleton/FormSkeletton'
 import type { TUser } from '../../../types/user.types'
 
 const UserProfile = () => {
+  const currentUser = useUserStore((state) => state.user)
   const setUser = useUserStore((state) => state.setUser)
 
   const { data, refetch, isError, isLoading } = useGetMe()
 
+  // use currentUser if exists or data from useGetMe hook
   useEffect(() => {
-    if (data?.data?.user == null) return
+    if (currentUser || data?.data?.user == null) return
 
-    // update user in ui state after fetch
+    // update user in ui state after fetch if no currentUser
     setUser(data?.data?.user)
-  }, [setUser, data?.data?.user])
+  }, [currentUser, setUser, data?.data?.user])
 
   if (isError) {
     return <RefetchButton refetch={refetch} />
   }
 
-  const user: TUser = data?.data?.user
+  const user: TUser = currentUser || data?.data?.user
 
   return (
     <>
