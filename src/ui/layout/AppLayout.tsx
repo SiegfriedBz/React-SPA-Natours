f38a4a01a852500e868a56e839af6bc74dc0c4be
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-24 13:41:21 +0200

    refactor AppLayout

--- src/ui/layout/AppLayout.tsx
+++ src/ui/layout/AppLayout.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { Outlet, ScrollRestoration, useMatch } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import Header from '../components/header/Header'
@@ -5,26 +6,13 @@ import Footer from '../components/Footer'
 import 'react-toastify/dist/ReactToastify.css'
 
 const AppLayout = () => {
-  const matchHome = useMatch('/')
-  const matchTours = useMatch('/tours')
-  const matchTourId = useMatch('/tours/:tourId')
-  const matchCreate = useMatch('/tours/new')
-  const matchUpdate = useMatch('/tours/:id/update')
-  const matchInsights = useMatch('/tours/insights')
-  const matchPlanning = useMatch('/tours/planning')
-  const isLoginPage = useMatch('/login')
-  const isSignupPage = useMatch('/signup')
-  const isBookingsPage = useMatch('/my-bookings')
-
-  const isHomePage = matchHome || matchTours
-  const isToursPage =
-    (isHomePage || matchTourId) &&
-    !matchCreate &&
-    !matchUpdate &&
-    !matchInsights &&
-    !matchPlanning
-
-  const isLoginOrSignupPage = isLoginPage || isSignupPage
+  const { isHomePage, isToursPage, isLoginOrSignupPage, isBookingsPage } =
+    useMatchPage()
+  const { getPageClasses } = usePageClasses({
+    isToursPage,
+    isLoginOrSignupPage,
+    isBookingsPage
+  })
 
   return (
     <div
@@ -33,18 +21,7 @@ const AppLayout = () => {
       "
     >
       <Header />
-      <main
-        className={`
-          ${
-            isToursPage
-              ? ''
-              : isLoginOrSignupPage
-                ? 'max-md:my-4 md:my-8'
-                : isBookingsPage
-                  ? 'min-h-[calc(100vh-var(--header-h))] max-sm:my-4 max-md:my-8 md:my-12'
-                  : 'max-sm:my-4 max-md:my-8 md:my-12'
-          }`}
-      >
+      <main className={getPageClasses()}>
         <Outlet />
       </main>
 
@@ -57,3 +34,67 @@ const AppLayout = () => {
 }
 
 export default AppLayout
+
+const useMatchPage = () => {
+  const matchHome = !!useMatch('/')
+  const matchTours = !!useMatch('/tours')
+  const matchTourId = !!useMatch('/tours/:tourId')
+  const matchCreate = !!useMatch('/tours/new')
+  const matchUpdate = !!useMatch('/tours/:id/update')
+  const matchInsights = !!useMatch('/tours/insights')
+  const matchPlanning = !!useMatch('/tours/planning')
+  const isLoginPage = !!useMatch('/login')
+  const isSignupPage = !!useMatch('/signup')
+  const isBookingsPage = !!useMatch('/my-bookings')
+
+  const isHomePage = matchHome || matchTours
+  const isToursPage =
+    (isHomePage || matchTourId) &&
+    !matchCreate &&
+    !matchUpdate &&
+    !matchInsights &&
+    !matchPlanning
+  const isLoginOrSignupPage = isLoginPage || isSignupPage
+
+  return { isHomePage, isToursPage, isLoginOrSignupPage, isBookingsPage }
+}
+
+type TUsePageClassesProps = {
+  isToursPage: boolean
+  isLoginOrSignupPage: boolean
+  isBookingsPage: boolean
+}
+const usePageClasses = ({
+  isToursPage,
+  isLoginOrSignupPage,
+  isBookingsPage
+}: TUsePageClassesProps) => {
+  const getClassesForLoginOrSignupPage = useCallback(() => {
+    const innerWidth = window.innerWidth
+    const upToXs = innerWidth < 404
+    const xsToSm = !upToXs && innerWidth < 640
+
+    const minHeightClass = upToXs
+      ? ''
+      : xsToSm
+        ? 'min-h-[calc(100vh-calc(2*var(--header-h)))]'
+        : 'min-h-[calc(100vh-var(--header-h))]'
+
+    return `max-md:my-4 md:my-8 ${minHeightClass} `
+  }, [])
+
+  const getPageClasses = useCallback(() => {
+    if (isToursPage) return ''
+    if (isLoginOrSignupPage) return getClassesForLoginOrSignupPage()
+    if (isBookingsPage)
+      return 'min-h-[calc(100vh-var(--header-h))] max-sm:my-4 max-md:my-8 md:my-12'
+    return 'max-sm:my-4 max-md:my-8 md:my-12'
+  }, [
+    isToursPage,
+    isLoginOrSignupPage,
+    isBookingsPage,
+    getClassesForLoginOrSignupPage
+  ])
+
+  return { getPageClasses }
+}
