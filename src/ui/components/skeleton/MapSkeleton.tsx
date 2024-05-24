9b10c8da4116fcc0e18bccd0326a7233de591d54
siegfriedbz <siegfried.bozza@yahoo.com>
2024-05-24 20:17:35 +0200

    fix TourDetailsSkeleton

--- src/ui/components/skeleton/MapSkeleton.tsx
+++ src/ui/components/skeleton/MapSkeleton.tsx
@@ -3,9 +3,12 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 const MapSkeleton = () => {
   return (
     <SkeletonTheme baseColor="#f3f3f3" highlightColor="#ecebeb">
-      <div className="clip-path-map w-full my-64 h-[48rem] flex items-center justify-center  ">
-        <div className="w-96 h-96 my-64 rounded-full overflow-hidden">
-          <Skeleton circle={true} width={400} height={400} />
+      <div className="w-full h-[100vw] min-h-[100vw] z-[9999] flex items-center justify-center">
+        <div className="h-[42svh] max-md:hidden md:-mt-64">
+          <Skeleton circle width={280} height={280} />
+        </div>
+        <div className="h-[42svh] md:hidden">
+          <Skeleton circle width={208} height={208} />
         </div>
       </div>
     </SkeletonTheme>
